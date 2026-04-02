570f4c2cd86467fe0d3bc24d594f42bbae120d39
Snaha19 <snahakundu19@gmail.com>
2026-04-02 20:12:29 +0530

    added q3 pattern problem

--- Recapitulating/pythonVsImport.py
+++ Recapitulating/pythonVsImport.py
@@ -1,6 +1,6 @@
 # python file.py is used to execute a Python file directly from the terminal, 
 # whereas import file is used to include another Python file as a module inside a program. 
 # Writing import file.py gives an error because Python does not allow file extensions in import statements.git add .
-git add .
-git commit -m "added q3 pattern problem"
-git push
+# git add .
+# git commit -m "added q3 pattern problem"
+# git push
